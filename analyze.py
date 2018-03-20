9a262d042bcf840ea64aa239498d1e15d5f9cbcb
yharaskrik <Wvkhrd77jkf04$>
2018-03-19 21:04:59 -0700

    improving algorithm

--- analyze.py
+++ analyze.py
@@ -2,15 +2,20 @@ import builder
 import calculate
 import file
 import filter
+import math
+import time
 import subgraph
 import networkx as nx
 from pprint import pprint
 import matplotlib.pyplot as plt
+from statistics import median, mean
 
 from plot import Plot
+import shapefile
 
 regions = {
-    # 'Global': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    # 'Subset': [10],
+    'Global': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
     'North America': [1],
     'Central America & Caribbean': [2],
     'South America': [3],
@@ -30,13 +35,22 @@ region_name = ['North America', 'Central America & Caribbean', 'South America', 
                'Southeast Asia', 'South Asia', 'Central Asia', 'Western Asia', 'Western Europe',
                'Eastern Europe', 'Middle East & North Africa', 'Sub-Saharan Africa', 'Australasia & Oceania']
 
-min_year = 2016
+min_year = 2000
+
+# number of countries, cells, locations, total number of attacks
+# how much data we actually used
+# ranked cities of most attacks (rank based on degree), and cells with most attacks (cell based graph, total degree)
+
+# pre 2000 post 2000 graphs cells as nodes and edges at shared attacks
+# average clustering coefficient
+# higher betweenness centrality
 
 r = file.File()
 f = filter.Filter()
 b = builder.Builder()
 c = calculate.Calculate()
 s = subgraph.SubGraph()
+p = Plot()
 
 # Read the dat into memory
 all_data = r.read_data('data.csv')
@@ -51,10 +65,115 @@ for key in regions:
 
     G = nx.DiGraph()
 
+    U_G = nx.Graph()
+
     # Filter out the data we do not want based on year and region
     # Also filters out missing and unknown data
     print('Filtering on region: ', key)
     data = f.filter(all_data, region=region)
+
+    groups = []
+    cities = []
+
+    group_location = {}
+
+    edge_dict = {}
+
+    print('Getting all lat and lon, adding city ndoes')
+    for index, row in data.iterrows():
+
+        lat = row['latitude']
+        lon = row['longitude']
+
+        if not lat or not lon:
+            continue
+
+        if row['city'] not in U_G:
+            U_G.add_node(row['city'], pos=(lon, lat), text=row['city'])
+            cities.append(row['city'])
+
+        if row['gname'] not in groups:
+            groups.append(row['gname'])
+
+        if row['gname'] not in group_location:
+            group_location[row['gname']] = {'cities': [row['city']], 'count': 1, 'lat': [lat], 'lon': [lon]}
+        elif row['city'] not in group_location[row['gname']]['cities']:
+            group_location[row['gname']]['cities'].append(row['city'])
+            group_location[row['gname']]['lat'].append(lat)
+            group_location[row['gname']]['lon'].append(lon)
+            group_location[row['gname']]['count'] += 1
+        else:
+            group_location[row['gname']]['count'] += 1
+
+        gname = row['gname']
+        city = row['city']
+
+        if city not in edge_dict:
+            edge_dict[city] = {gname: 1}
+        else:
+            if gname not in edge_dict[city]:
+                edge_dict[city][gname] = 1
+            else:
+                edge_dict[city][gname] += 1
+
+    Group_G = nx.Graph()
+
+    # print(group_location)
+    print(groups)
+
+    for item in group_location:
+        # print(item)
+        # print(group_location[item])
+        avg_lat = mean(group_location[item]['lat'])
+        avg_lon = mean(group_location[item]['lon'])
+        Group_G.add_node(item, pos=(avg_lon, avg_lat), text=item)
+
+    p.fr2(Group_G)
+
+    break
+
+    print(U_G.nodes)
+    count = 0
+    print('Data Length:', len(data))
+    print('Group length: ', len(groups))
+    print('Locations length: ', len(cities))
+    for group in groups:
+        sub_data = data[data['gname'] == group]
+
+        for outer_index in range(0, len(sub_data) - 1):
+            outer_row = sub_data.iloc[[outer_index]]
+            outer_city = outer_row['city'].values[0]
+            for inner_index in range(outer_index + 1, len(sub_data)):
+                inner_row = sub_data.iloc[[inner_index]]
+                inner_city = inner_row['city'].values[0]
+                if outer_city != inner_city:
+                    if outer_city not in U_G[inner_city]:
+                        U_G.add_weighted_edges_from([(outer_city, inner_city, 1)])
+                    else:
+                        U_G[outer_city][inner_city]['weight'] += 1
+
+                count += 1
+                if count % 1000 == 0:
+                    print(time.time())
+                    print(len(groups) * len(data) * math.log(len(data), 10), ' - ', count)
+
+    print('#Edges:', len(U_G.edges))
+    avg_edge_weight = sum([U_G.get_edge_data(*edge)['weight'] for edge in U_G.edges]) / len(U_G.edges)
+    cut_off = sorted([U_G.get_edge_data(*edge)['weight'] for edge in U_G.edges])[-int(len(U_G.edges) * 0.2):]
+    mean_weight = mean(cut_off)
+    median_weight = median(cut_off)
+    print(median(cut_off))
+    print(mean(cut_off))
+    print(avg_edge_weight)
+
+    edges_to_remove = [edge for edge in U_G.edges if U_G.get_edge_data(*edge)['weight'] < mean_weight]
+    U_G.remove_edges_from(edges_to_remove)
+    # U_G.remove_nodes_from(list(nx.isolates(U_G)))
+    print('#Remove: ', len(edges_to_remove))
+
+    print(len(U_G.edges))
+    p.fr2(U_G, cities)
+    break
 
     if key == 'Global':
         print('Creating plot for the global terrorist network')
--- plot.py
+++ plot.py
@@ -2,8 +2,128 @@ import networkx as nx
 import plotly.plotly as py
 import plotly
 from plotly.graph_objs import *
+import shapefile
 
 class Plot:
+
+    def fr2(self, G, nodes=None):
+        axis = dict(showline=False,  # hide axis line, grid, ticklabels and  title
+                    zeroline=False,
+                    showgrid=False,
+                    showticklabels=False,
+                    title=''
+                    )
+        layout = Layout(title="Terrorist attack locations",
+                        font=Font(size=12),
+                        showlegend=False,
+                        autosize=False,
+                        width=2000,
+                        height=2000,
+                        xaxis=XAxis(axis),
+                        yaxis=YAxis(axis),
+                        margin=Margin(
+                            l=40,
+                            r=40,
+                            b=85,
+                            t=100,
+                        ),
+                        hovermode='closest',
+                        annotations=Annotations([
+                            Annotation(
+                                showarrow=False,
+                                text='This igraph.Graph has the Kamada-Kawai layout',
+                                xref='paper',
+                                yref='paper',
+                                x=0,
+                                y=-0.1,
+                                xanchor='left',
+                                yanchor='bottom',
+                                font=Font(
+                                    size=14
+                                )
+                            )
+                        ]),
+                        )
+        pos = nx.get_node_attributes(G, 'pos')
+        N = G.nodes
+        E = G.edges
+        Xv = [pos[k][0] for k in N]
+        Yv = [pos[k][1] for k in N]
+        Xed = []
+        Yed = []
+        for edge in E:
+            Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
+            Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+        trace3 = Scatter(x=Xed,
+                         y=Yed,
+                         mode='lines',
+                         line=Line(color='rgb(210,0,0)', width=1),
+                         hoverinfo='none'
+                         )
+        trace4 = Scatter(x=Xv,
+                         y=Yv,
+                         mode='markers',
+                         name='net',
+                         marker=Marker(symbol='dot',
+                                       size=5,
+                                       color='#6959CD',
+                                       line=Line(color='rgb(50,50,50)', width=0.5)
+                                       ),
+                         text=nodes,
+                         hoverinfo='text'
+                         )
+
+        sf = shapefile.Reader('TM_WORLD_BORDERS_SIMPL')
+
+        shapes = sf.shapes()
+
+        shape_graph = nx.Graph()
+        for shape in shapes:
+            for point_num in range(0, len(shape.points)):
+                shape_graph.add_node(str(shape.points[point_num]), pos=shape.points[point_num])
+            for point_num in range(0, len(shape.points) - 1):
+                shape_graph.add_edge(str(shape.points[point_num]), str(shape.points[point_num + 1]))
+
+        pos = nx.get_node_attributes(shape_graph, 'pos')
+        N = shape_graph.nodes
+        E = shape_graph.edges
+        Xv = [pos[k][0] for k in N]
+        Yv = [pos[k][1] for k in N]
+        Xed = []
+        Yed = []
+        for edge in E:
+            Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
+            Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+        trace5 = Scatter(x=Xed,
+                         y=Yed,
+                         mode='lines',
+                         line=Line(color='rgb(210,210,210)', width=1),
+                         hoverinfo='none'
+                         )
+        trace6 = Scatter(x=Xv,
+                         y=Yv,
+                         mode='markers',
+                         name='net',
+                         marker=Marker(symbol='dot',
+                                       size=5,
+                                       color='#6959CD',
+                                       line=Line(color='rgb(50,50,50)', width=0.5)
+                                       ),
+                         text=nodes,
+                         hoverinfo='none'
+                         )
+
+        print('Total edges in border graph', len(shape_graph.edges))
+
+        annot = "This networkx.Graph has the Fruchterman-Reingold layout<br>Code:" + \
+                "<a href='http://nbviewer.ipython.org/gist/empet/07ea33b2e4e0b84193bd'> [2]</a>"
+
+        data1 = Data([trace3, trace4, trace5])
+        fig1 = Figure(data=data1, layout=layout)
+        fig1['layout']['annotations'][0]['text'] = annot
+        plotly.offline.plot(fig1, filename='Coautorship-network-nx.html')
 
     def fruchterman_reingold(self, G):
         # G = nx.random_geometric_graph(200, 0.125)
